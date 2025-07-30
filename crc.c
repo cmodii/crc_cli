d54e70f4869aa3b1727567da7a1f713b165a42a9
cmodii <100476911+cmodii@users.noreply.github.com>
2025-07-30 18:38:24 +0100

    Calculate through lookup table

--- crc.c
+++ crc.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 #include <string.h>
+#define CR8_POLYNOMIAL 0x7
+
+unsigned int CRC8_table[256];
+
+void calculate_CRC8_table() {
+    unsigned int currCRC;
+    for (unsigned int dividend = 0; dividend < 256; dividend++) {
+        currCRC = dividend;
+
+        for (int i = 0; i < 8; i++) {
+            if ((currCRC & 0x80) != 0) {
+                currCRC = (unsigned int) ((currCRC << 1) & 0xFF) ^ CR8_POLYNOMIAL;
+            } else {
+                currCRC = (currCRC & 0xFF) << 1;
+            }
+        }
+
+        CRC8_table[dividend] = currCRC;
+    }
+}
 
 void print_binary(unsigned int number)
 {
@@ -10,31 +30,30 @@ void print_binary(unsigned int number)
 }
 
 unsigned int crc8(char *bytes) {
-    const unsigned int polynomial = 0x7;
     unsigned int crc = 0;
 
-    for (int j = 0; j < strlen(bytes); j++) {
-        crc ^= (unsigned int) *(bytes+j); // XOR with current byte
-
-        for (int i = 0; i < 8; i++) {
-            if ((crc & 0x80) != 0) { // is MSB set?
-                crc = (unsigned int) (crc << 1) ^ polynomial; // XOR with divisor
-            } else {
-                crc = (unsigned int) crc << 1; // otherwise push incoming stream
-            }
-        }
+    for (int i = 0; i < strlen(bytes); i++) {
+        unsigned int data = ((unsigned int) bytes[i]) ^ (crc & 0xFF);
+        crc = CRC8_table[data];
     }
 
-    crc &= 0xFF; // register must be fixed on 8 bits
     return crc;
 }
 
 int main() {
-    // e.g: getting the crc of each byte in the input stream "Hello"
+    // e.g: getting the crc of each byte in the input stream "Hello, World!"
+    calculate_CRC8_table(); // expensive
+
+    for (int i = 0; i < 256; i++) {
+        printf("[%X] ", CRC8_table[i]);
+        if (i % 16 == 0 && i > 1) {printf("\n");}
+    }
+    printf("\n");
+
     char data[] = "Hello, World!";
     unsigned int crc = crc8(data);
 
     printf("[Data]: %s\n", data);
-    printf("[CRC]: %X |", crc);
+    printf("[CRC]: %X | ", crc);
     print_binary(crc);
 }
