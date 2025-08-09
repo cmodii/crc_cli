0c22008d51fe3ef071e15835e3c693f874c7de30
cmodii <100476911+cmodii@users.noreply.github.com>
2025-08-09 11:24:03 +0100

    Colorful output

--- src/main.c
+++ src/main.c
@@ -4,18 +4,14 @@
 
 int main(int argc, char *argv[]) {
     if (argc < 3) {
-        printf("Incorrect format. Usage: %s <crc8|crc16|crc32> \"input string\"", argv[0]);
+        printf("\033[31mIncorrect format.\033[0m Usage: \033[32m%s <crc8|crc16|crc32> \"input string\"\033[0m", argv[0]);
         return 1;
-    }
-
-    if (argc > 3) {
-        printf("Note: for space separated text use quotation marks: ""input stream""\n");
     }
     
     const char *mode = argv[1];
     const char *input = argv[2];
     unsigned int CRC, poly, start_value;
-
+    
     if (!strcmp("crc8", mode)) {
         CRC = crc8(input, strlen(input));
         poly = CRC_8_POLYNOMIAL;
@@ -29,10 +25,14 @@ int main(int argc, char *argv[]) {
         poly = CRC_32_POLYNOMIAL;
         start_value = CRC32_START_VALUE;
     } else {
-        printf("Specify a correct mode: <crc8|crc16|crc32>");
+        printf("\033[33mSpecify a correct mode: <crc8|crc16|crc32>\033[0m");
         return 1;
     }
-
-    printf("[Input stream]: %s\n[CRC]: 0x%X\n[Polynomial]: 0x%X\n[CRC initial value]: %X", input, CRC, poly, start_value);
+    
+    printf("\033[34m[Input stream]:\033[0m \033[32m%s\033[0m\n\033[34m[CRC]:\033[0m \033[32m0x%X\033[0m\n\033[34m[Polynomial]:\033[0m 0x%X\n\033[34m[CRC initial value]:\033[0m %X\n\n", input, CRC, poly, start_value);
+    
+    if (argc > 3) {
+        printf("\033[33mNote: for space separated text use quotation marks. e.g: cli crc32 \"Sic Parvis Magna\"\033[0m\n", 33);
+    }
     return 0;
 }
